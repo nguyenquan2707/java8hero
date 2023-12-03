1ea61ec190db5d12b8ec382f0025d91b3aacc631
Quan Nguyen <vozermuonnam1@gmail.com>
2023-12-03 14:57:09 +0700

    lambd

--- src/main/java/quan/Lambda.java
+++ src/main/java/quan/Lambda.java
@@ -5,20 +5,6 @@ interface Shape {
     void draw();
 }
 
-class Rectangle implements Shape {
-
-    @Override
-    public void draw() {
-        System.out.println("Rectangle....");
-    }
-}class Square implements Shape {
-
-    @Override
-    public void draw() {
-        System.out.println("Square....");
-    }
-}
-
 public class Lambda {
     public static void main(String[] args) {
             Shape rectangle = () -> System.out.println("Rectangle....");
