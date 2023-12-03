43cd7dba121870542ea8f04bd4850e9809aba13f
Quan Nguyen <vozermuonnam1@gmail.com>
2023-12-03 20:50:08 +0700

    pass lambda or implement into parameter of method

--- src/main/java/quan/Lambda.java
+++ src/main/java/quan/Lambda.java
@@ -8,6 +8,15 @@ interface Shape {
 public class Lambda {
     public static void main(String[] args) {
             Shape rectangle = () -> System.out.println("Rectangle....");
-            rectangle.draw();
+//            rectangle.draw();
+
+//            print(rectangle);
+
+        //put implement directly
+        print(() -> System.out.println("Rectangle..."));
+    }
+
+    private static void print(Shape shape) {
+        shape.draw();
     }
 }
---
+++ src/main/java/quan/LambdaParameter.java
@@ -0,0 +1,22 @@
+package quan;
+
+
+interface Addable {
+    int add(int a, int b);
+}
+
+class AddableImpl implements Addable {
+
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+public class LambdaParameter {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
