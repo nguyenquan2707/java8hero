47de020c3c071d9deef87ad4241a179ac1be6baf
Quan Nguyen <vozermuonnam1@gmail.com>
2023-12-22 20:26:58 +0700

    min max

--- src/main/java/quan/find/FindDemo.java
+++ src/main/java/quan/find/FindDemo.java
@@ -3,6 +3,7 @@ package quan.find;
 import quan.stream.Product;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +27,24 @@ public class FindDemo {
         long count = getProducts().stream()
                 .count();
         System.out.println(count);
+
+        System.out.println("min....");
+        //min
+        getProducts().stream()
+                .min(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product o1, Product o2) {
+                        return (int) ((int) o1.getPrice() - o2.getPrice());
+                    }
+                }).ifPresent(System.out::println);
+
+        getProducts().stream()
+                .max(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product o1, Product o2) {
+                        return (int) ((int) o1.getPrice() - o2.getPrice());
+                    }
+                }).ifPresent(System.out::println);
     }
 
     private static List<Product> getProducts() {
